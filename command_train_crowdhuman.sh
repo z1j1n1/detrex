5fcbae8c6181e8f37234ab33304363f9934100d0
z1j1n1 <905005832@qq.com>
2023-07-10 16:22:10 +0800

    upd

---
+++ command_train_crowdhuman.sh
@@ -0,0 +1,2 @@
+export DETECTRON2_DATASETS=/data/jiangning/coco_link/
+python tools/train_net.py --config-file projects/dino/configs/dino-vitdet/dino_vitdet_base_4scale_12ep_crowdhuman.py --num-gpus 8
